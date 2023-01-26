7379026dbc541f7847619bb598f532966e4b16b6
Olivia Gilbert <umhello2020@gmail.com>
2023-01-26 14:44:54 -0600

    polished some functions in JS, trying to rework api, polishing CSS

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 // Global Variable
 let searchHistory = JSON.parse(window.localStorage.getItem('searchHistory')) || [];
+
 // DOM Variables
 let searchBtn = document.getElementById('searchBtn');
-// let recentCityBtn = document.getElementById('recent-city-btn')
 let searchHistoryContainer = document.getElementById('search-history-container');
 let cityInputEl = document.getElementById('city-input');
 let currentDayEl = document.getElementById('current-day');
 let multiForecast = document.getElementById('multi-day-forecast');
+let tempTodayEl = document.getElementById('temp-today');
+let humidityTodayEl = document.getElementById('humidity-today');
+let windTodayEl = document.getElementById('wind-today');
 
-
-// Timezone pluggins for day.js
+// Plugins for timezones
 // dayjs.extend(window.js_plugin_utc);
 // dayjs.extend(window.dayjs_plugin_timezone);
 
+// Loads search history to page upon load
+function onLoad () {
+    searchHistoryContainer.innerHTML = '';
+    createHistoryBtn()
+}
+
+// Creates history buttons
+function createHistoryBtn() {
+    for (let i = 0; i < searchHistory.length; i++) {
+        let liEl = document.createElement('li');
+        let historyBtn = document.createElement('button');
+        historyBtn.setAttribute('type', 'button');
+        historyBtn.setAttribute('id', 'history-btn');
+        historyBtn.setAttribute('data-value', searchHistory[i]);
+        historyBtn.addEventListener('click', function (e) {
+            if (e.target.dataset.value === searchHistory[i]) {
+                console.log(e.target.dataset.value)
+                cityData(searchHistory[i]);
+                
+                cityInputEl.value = searchHistory[i];
+            }
+        });
+        
+        historyBtn.textContent = searchHistory[i];
+        liEl.appendChild(historyBtn);
+
+        searchHistoryContainer.appendChild(liEl);
+        
+    }
+}
+
+// Retrieves coordinates from city input name
 function cityData(city) {
  fetch("https://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=5f03a7ebe75741bbe3cd6f91f18b0bd7")
     .then(function (response) {
@@ -21,112 +55,60 @@ function cityData(city) {
     .then(function(data) {
         let currentLat = data[0].lat
         let currentLon = data[0].lon
-        currentDay(currentLat, currentLon)
+        // currentDay(currentLat, currentLon)
         currentForecast(currentLat, currentLon);
-        console.log(data);
     })
 }
 
+// Retrieves 5-day forecast from coordinates
 function currentForecast(lat, lon) {
     fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&units=imperial&appid=5f03a7ebe75741bbe3cd6f91f18b0bd7")
     .then(function (response) {
         return response.json();
       })
     .then(function(data) {
+        let todaysInfo = data.list[0];
+        //displayToday(todaysInfo);
+        console.log(todaysInfo);
         for (let i = 0; i < data.list.length; i+=8) {
             
         }
     })
 }
 
-function currentDay (lat, lon) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=imperial&appid=5f03a7ebe75741bbe3cd6f91f18b0bd7")
-    .then(function (response) {
-        return response.json();
-      })
-      .then(function(data) {
-       // displayToday(data)
-    })
-}
+// Displays current dates weather information
+// function displayToday(todaysInfo) {
+//     // let date = dayjs().format('M/D/YYYY');
 
-//function displayToday(data) {
-//     tempTodayEl.innerHTML = data.main.temp + "°F"
-//     humidityTodayEl.innerHTML = data.main.humidity + "%"
-//     windCurrent.innerHTML = data.wind.speed + " " + "mph"
-//     localStorage.setItem("cityInput", data);
-//     console.log(weatherInfo);
+//     let cityName = document.getElementById('city-name');
+//     let city = cityInputEl.value
+//     cityName.innerHTML = city.toUpperCase();
+
+//     tempTodayEl.innerHTML = todaysInfo.main.temp + "°F"
+//     humidityTodayEl.innerHTML = todaysInfo.main.humidity + "%"
+//     windTodayEl.innerHTML = todaysInfo.wind.speed + " " + "mph"
 // }
 
-function onLoad () {
-    // let searchHistory = JSON.parse(window.localStorage.getItem('searchHistory'))
+// function displayForecast()
 
-    if (searchHistory.length !== 0) {
-        for (let i = 0; i < 5; i++) {
-            let liEl = document.createElement('li');
-            let historyBtn = document.createElement('button');
-            historyBtn.setAttribute('type', 'button');
-            historyBtn.setAttribute('id', 'recent-city-btn');
-            historyBtn.setAttribute('data-value', searchHistory[i]);
-            historyBtn.addEventListener('click', function (e) {
-                if (e.target.dataset.value === searchHistory[i]) {
-                    console.log(e.target.dataset.value)
-                    cityData(searchHistory[i]);
-                    
-                    cityInputEl.value = searchHistory[i];
-                }
-            });
-            
-            historyBtn.textContent = searchHistory[i];
-            liEl.appendChild(historyBtn);
 
-            searchHistoryContainer.appendChild(liEl);
-            
-        }
-    }
-}
-
-function saveSearch (cityInput) {
-    let city = cityInput.trim();
+function saveSearch (city) {
+    let cityInput = city.trim();
     searchHistoryContainer.innerHTML = "";
 
-    if (city !== '') {
+    if (cityInput !== '') {
 
-        searchHistory.push(city);
+        searchHistory.push(cityInput);
         window.localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
     
-    
-        for (let i = 0; i < searchHistory.length; i++) {
-            let liEl = document.createElement('li');
-            let historyBtn = document.createElement('button');
-            historyBtn.setAttribute('type', 'button');
-            historyBtn.setAttribute('id', 'recent-city-btn');
-            historyBtn.setAttribute('data-value', searchHistory[i]);
-            historyBtn.addEventListener('click', function (e) {
-                if (e.target.dataset.value === searchHistory[i]) {
-                    console.log(e.target.dataset.value)
-                    cityData(searchHistory[i]);
-                    
-                    cityInputEl.value = searchHistory[i];
-                }
-            });
-            
-            historyBtn.textContent = searchHistory[i];
-            liEl.appendChild(historyBtn);
-
-            searchHistoryContainer.appendChild(liEl);
-            
-        }
+        createHistoryBtn();
     }
 }
 
 searchBtn.addEventListener('click', function(){
-    let cityInput = document.getElementById('city-input').value;
-    console.log(cityInput);
-    cityData(cityInput);
-    saveSearch(cityInput);
+    let city = cityInputEl.value
+    cityData(city);
+    saveSearch(city);
 });
  
 onLoad();
-// recentCityBtn.addEventListener('click', saveSearch);
-
-// local storage function create an array and then create for loop inside with create button and append page
